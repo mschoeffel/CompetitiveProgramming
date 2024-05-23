63594ea9367f2bd0c7f2548c07803ed19850244e
Michael Schoeffel <michael.schoeffel@tripsync.net>
2024-05-23 17:21:25 +0200

    Fixed LeetCode - Go - Lowest Common Ancestor of a Binary Search Tree

--- LeetCode/Go/X0235_LowestCommonAncestorOfABinarySearchTree/App.go
+++ LeetCode/Go/X0235_LowestCommonAncestorOfABinarySearchTree/App.go
@@ -12,7 +12,8 @@ type TreeNode struct {
 }
 
 func Main() {
-	root := &TreeNode{Val: 1}
+
+	root := &TreeNode{Val: 6, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 0}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 5}}}, Right: &TreeNode{Val: 8, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 9}}}
 	p := &TreeNode{Val: 2}
 	q := &TreeNode{Val: 8}
 
